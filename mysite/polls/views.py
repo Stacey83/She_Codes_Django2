16e3e8b9a2b6b950f217a37f186fa4359c91a5cf
Stacey83 <staceyhill83@hotmail.com>
2021-08-13 21:17:44 +1000

    Database created

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.urls import generic  #generic not in django URLs file
+from django.views import generic
 
 from .models import Choice, Question
 # Create your views here.
